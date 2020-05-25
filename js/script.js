521b4af1f1e1ad663df475cd02ac71b891b2b34d
Francesco <sittafrancesco2020@gmail.com>
2020-05-25 17:32:09 +0200

    mail request

--- js/script.js
+++ js/script.js
@@ -2,25 +2,27 @@
 // creare la lista emailù
 
 var emaillist = ["francesco@fmail.com", "giovanna@fmail.com", "stefano@fmail.com", "margherita@fmail.com", "alexandra@fmail.com"];
-for ( var i = 0; i < emaillist.lenght; i++ ) {
+for ( var i = 0; i < emaillist.lenght; i++) {
   conosle.log(emaillist[i]);
 }
 
 // variabile per la verifica
 
-var acessmail = false;
+var accessmail = false;
+
+//emailuser
+var mailrequest = prompt('Qual\'è la tua mail??');
 
 for ( var i = 0; i < emaillist.lenght; i++ ) {
-  if(emaillist[i] == email) {
-    acessmail = true;
+  if(emaillist[i] === mailrequest) {
+    accessmail = true;
   }
 }
 
-if(acessmail) {
-  alert('Accesso eseguito!')
-} else {
-  alert('Accesso errato!')
-}
+if(mailrequest === emaillist) {
+  alert('Accesso eseguito correttamente!');
+} else if (mailrequest |= emaillist)
+  alert('Accesso errato!');
 
 // fine primo-esercizio
 
@@ -29,7 +31,7 @@ var numeroumano = Math.floor(Math.random() *6) +1;
 prompt('Scrivi un numero da 1 a 6:');
 
 var numeropc = Math.floor(Math.random() *6) +1;
-alert('Il numero scelto dal pc è: '+ numeropc);
+alert('Il numero scelto dal pc è: ' + numeropc);
 
 //Formula per il vincitore
 if(numeroumano > numeropc) {
